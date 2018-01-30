e4dc35401db42d8d8b132129e175270e673337d8
Tlenc <tlenciauskas@gmail.com>
2018-01-30 16:15:18 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Įvedimas();
+
+
+        System.out.println("Vidurkis :" + Vidurkis());
     }
+    static float km;
+    static float ks;
+    public static void Įvedimas(){
+        System.out.println("Įveskite nuvažiuotus km ir kuro sąnaudas");
+        Scanner sc = new Scanner(System.in);
+         km = sc.nextFloat();
+         ks = sc.nextFloat();
+
+    }
+    public static float Vidurkis(){
+        return ks*100/km;
+
+    }
+
+
 }
